4b83f36f15bee6dbe9ed6e21be6867f750780741
Alex Straight <alexstraight619@gmail.com>
2026-05-09 09:54:25 -0700

    chore: remove root.zig and reference to root in build.zig

--- client/build.zig
+++ client/build.zig
@@ -4,11 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("zwag", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-
     const vaxis = b.dependency("vaxis", .{
         .target = target,
         .optimize = optimize,
@@ -19,7 +14,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .imports = &.{
-            .{ .name = "zwag", .module = mod },
             .{ .name = "vaxis", .module = vaxis.module("vaxis") },
         },
     });
@@ -40,19 +34,11 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
-
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
